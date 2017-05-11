a36d69e6e6c8cdccf42010c09a8bcdfde88a2411
nicol <nicol@HAKABOB>
2017-05-11 16:36:03 +0200

    dzega

--- Skarp/Skarp/forms/Form_Create_Arbo.cs
+++ Skarp/Skarp/forms/Form_Create_Arbo.cs
@@ -12,12 +12,26 @@ namespace Skarp.forms
 {
     public partial class Form_Create_Arbo : Form
     {
+        string game;
         Team forGettingAllTeams;
         DataSet dataAllTeam;
+        Tournament allTournaments;
+        DataSet dataSetAllTournaments;
         public Form_Create_Arbo()
         {
             InitializeComponent();
-            
+            allTournaments = new Tournament();
+            dataSetAllTournaments = allTournaments.getTournamentOfTheSession();
+
+            foreach (DataRow rw in dataSetAllTournaments.Tables["tournament"].Rows)
+            {
+
+                    cbb_Name_Tournament.Items.Add(rw["name"].ToString());
+                
+
+            }
+
+
             forGettingAllTeams = new Team();
         }
 
@@ -27,6 +41,34 @@ namespace Skarp.forms
             foreach ( DataRow r in dataAllTeam.Tables[0].Rows)
             {
 
+                cbR1T1.Items.Add(r["name"].ToString());
+                cbR1T2.Items.Add(r["name"].ToString());
+                cbR1T3.Items.Add(r["name"].ToString());
+                cbR1T4.Items.Add(r["name"].ToString());
+                cbR1T5.Items.Add(r["name"].ToString());
+                cbR1T6.Items.Add(r["name"].ToString());
+                cbR1T7.Items.Add(r["name"].ToString());
+                cbR1T8.Items.Add(r["name"].ToString());
+
+            }
+        }
+
+        private void cbb_Name_Tournament_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (DataRow rw in dataSetAllTournaments.Tables["tournament"].Rows)
+            {
+
+                if (rw["name"].ToString() == cbb_Name_Tournament.Text)
+                {
+                    game = Convert.ToString(rw["jeu"]);
+                    break;
+                }
+
+            }
+            dataAllTeam = forGettingAllTeams.getAllTeam();
+            foreach (DataRow r in dataAllTeam.Tables[0].Rows)
+            {
+                if (r["jeu"].ToString()== game)
                 cbR1T1.Items.Add(r["name"].ToString());
                 cbR1T2.Items.Add(r["name"].ToString());
                 cbR1T3.Items.Add(r["name"].ToString());
