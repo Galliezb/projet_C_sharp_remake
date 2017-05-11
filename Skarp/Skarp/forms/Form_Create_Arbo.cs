6e0a3256dc8d640485335a6db50959e8e034c4db
Enigmaalz <Enigmaalz@ENIGMAAL>
2017-05-11 16:07:45 +0200

    arbo

---
+++ Skarp/Skarp/forms/Form_Create_Arbo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Skarp.forms
+{
+    public partial class Form_Create_Arbo : Form
+    {
+        Team forGettingAllTeams;
+        DataSet dataAllTeam;
+        public Form_Create_Arbo()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form_Create_Arbo_Load(object sender, EventArgs e)
+        {
+            dataAllTeam = forGettingAllTeams.getAllTeam();
+            dataAllTeam.Tables[0].Columns["name"].
+        }
+    }
+}
